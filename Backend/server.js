5f8cdb7b037a28e90ca454868802473c05ec80ed
Gracy Chauhan <gracychauhan17@gmail.com>
2026-08-17 12:37:46 +0530

    initialized the Project Management System

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app=express();
+const PORT=5000;
+app.get("/",(req,res)=>{
+    res.send("Taskflow Backend is running!");
+})
+app.listen(PORT,()=>{
+    console.log(`server is running on port${PORT}`)
+})
